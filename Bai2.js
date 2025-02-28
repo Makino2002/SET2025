e5623e03de62b142b751797d8dfdb73423a75aa3
Makino2002 <Maiphuc455@gmail.com>
2025-02-28 16:28:21 +0700

    fixing for function

--- Bai2.js
+++ Bai2.js
@@ -6,15 +6,15 @@ function BMI(weight, height) {
 // Calculate Simple Interest
 console.log("BMI: " + BMI(weight, height));
 
-let Principal = 100000;
-let Rate = 3.875;
-let Time = 5;
-function calculatorSimpleInterest(Principal, Rate, Time) {
+let principal = 100000;
+let rate = 3.875;
+let time = 5;
+function calculateSimpleInterest(trincipal, tate, time) {
   return Principal * (1 + Rate * Time);
 }
 console.log(
   "Calculating Simple Interest: " +
-    calculatorSimpleInterest(Principal, Rate, Time)
+    calculateSimpleInterest(Principal, Rate, Time)
 );
 
 let USD = 10;
@@ -23,13 +23,13 @@ function convertCurrency(USD) {
 }
 console.log(USD + " USD = " + convertCurrency(USD) + " VND");
 
-function CalculateTime(seconds) {
+function calculateTime(seconds) {
   let hours = 0;
   let minutes = 0;
-  if ((seconds) => 60) {
+  if (seconds >= 60) {
     minutes = Math.floor(seconds / 60);
     seconds = seconds - minutes * 60;
-    if ((minutes) => 60) {
+    if (minutes >= 60) {
       hours = Math.floor(minutes / 60);
       minutes = minutes - hours * 60;
     }
@@ -40,4 +40,4 @@ function CalculateTime(seconds) {
   console.log(seconds);
 }
 let seconds = 3 * 60 * 60 + 4 * 60 + 30;
-console.log(seconds + "Giây => " + CalculateTime(seconds));
+console.log(seconds + "seconds => " + calculateTime(seconds));
